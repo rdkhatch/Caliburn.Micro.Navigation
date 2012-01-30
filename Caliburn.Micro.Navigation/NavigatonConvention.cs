7df56f6c2ae3f2ce1b4fabc04a87a8c70473b4f1
Ryan D. Hatch <ryan@geniuscode.net>
2012-01-30 09:28:53 -0600

    Upgraded to latest Caliburn.Micro & Autofac Nuget packages
    Fixed NavigationConvention

--- Caliburn.Micro.Navigation/NavigatonConvention.cs
+++ Caliburn.Micro.Navigation/NavigatonConvention.cs
@@ -6,9 +6,9 @@ namespace Caliburn.Micro.Navigation
 {
     public static class NavigationConvention
     {
-        public static void  AddNavigationConventions()
+        public static void AddNavigationConventions()
         {
-            var conv=ConventionManager.AddElementConvention<Frame>(ContentControl.ContentProperty, "DataContext", "Loaded");
+            var conv = ConventionManager.AddElementConvention<Frame>(ContentControl.ContentProperty, "DataContext", "Loaded");
             conv.GetBindableProperty =
                 delegate(DependencyObject foundControl)
                 {
@@ -17,21 +17,25 @@ namespace Caliburn.Micro.Navigation
                         ? NavigationView.ModelProperty
                         : ContentControl.ContentProperty;
                 };
+
             conv.ApplyBinding = (viewModelType, path, property, element, convention) =>
             {
-                if (!ConventionManager.SetBinding(viewModelType, path, property, element, convention))
-                    return;
+                if (!ConventionManager.SetBindingWithoutBindingOverwrite(viewModelType, path, property, element, convention, convention.GetBindableProperty((DependencyObject)element)))
+                    return false;
+
                 var frame = element as Frame;
-                if (!typeof(NavigationConductor).IsAssignableFrom(viewModelType) || frame==null)
-                    return;
-                var binding = new Binding("FrameSource") {Mode = BindingMode.TwoWay};
+                if (!typeof(NavigationConductor).IsAssignableFrom(viewModelType) || frame == null)
+                    return true;
+
+                var binding = new Binding("FrameSource") { Mode = BindingMode.TwoWay };
                 BindingOperations.SetBinding(frame, Frame.SourceProperty, binding);
-                var calLoader=frame.ContentLoader as CaliburnContentLoader;
-                if(calLoader==null) return;
-                var contentLoaderBinder = new Binding("DataContext") {Source = frame,Mode=BindingMode.OneWay};
+                var calLoader = frame.ContentLoader as CaliburnContentLoader;
+                if (calLoader == null) return true;
+
+                var contentLoaderBinder = new Binding("DataContext") { Source = frame, Mode = BindingMode.OneWay };
                 BindingOperations.SetBinding(calLoader, CaliburnContentLoader.NavigationConductorProperty,
                                              contentLoaderBinder);
-
+                return true;
             };
         }
     }
